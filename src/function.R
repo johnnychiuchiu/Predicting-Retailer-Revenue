0116e364dcf8fa41f2ca7c9d253ecd88da3fdaf8
Johnny Chiu <johnnychiu@Johnnyde-MacBook-Pro.local>
2018-01-21 02:18:18 -0600

    add report and html

--- src/function.R
+++ src/function.R
@@ -5,7 +5,8 @@ library(car)
 library(MASS)
 library(glmnet)
 library(pROC)
-library(DAAG)
+library(knitr)
+options(scipen=999) # remove scientific notation in printing
 
 check_outlier <- function(df, name, sd_cutoff){
   # Using standard deviation to check the outlier for numerical columns
@@ -25,7 +26,7 @@ check_outlier <- function(df, name, sd_cutoff){
   print(paste('Sd of',name,':',sd))
   condition = paste(name, '<', mean , '-' , sd_cutoff, '*', sd, '|', 
                     name, '>', mean , '+' , sd_cutoff, '*', sd, sep='' )
-  View(df %>% filter_(condition))
+  df %>% filter_(condition)
 }
 
 rm_outlier <- function(df, name, sd_cutoff){
@@ -47,6 +48,14 @@ rm_outlier <- function(df, name, sd_cutoff){
                     name, '<=', mean , '+' , sd_cutoff, '*', sd, sep='' )
   return(df %>% filter_(condition))
 }
+
+replace_outlier <- function(df, name, sd_cutoff){
+  df_filtered = rm_outlier(df, name, sd_cutoff)
+  
+  df[,name] = ifelse(df[,name]>sd_cutoff, max(df_filtered[,name]), df[,name])
+  return(df)
+}
+
 
 data_manipulate <- function(df){
   # A function that does several data preprocessing
@@ -82,22 +91,35 @@ feature_engineer <- function(df, isTrain=TRUE){
   offer_date = as.Date('01AUG2014',"%d%b%Y")
   df_last_purchase_time = df %>% group_by(id) %>% summarise(max_date = max(orddate), min_date = min(orddate)) %>% 
                               mutate(days_recent_purchase = as.integer(offer_date-max_date),
-                                     days_first_purchase = as.integer(offer_date-min_date)) %>%
-                              dplyr::select(id, days_recent_purchase, days_first_purchase)
+                                     days_first_purchase = as.integer(offer_date-min_date)) %>% 
+                              mutate(total_duration = days_first_purchase - days_recent_purchase) %>%
+                              dplyr::select(id, days_recent_purchase, days_first_purchase, total_duration)
   
   ### Frequency
   # number of orders
   df_order_count = df %>% group_by(id) %>% summarise(order_count = n_distinct(ordnum))
   
-  # # qty sum of each category for each id -> Using it generate worse result for testset on Kaggle, even though it increase the R^2 in train a lot. Can not verify it using cv.lm either. Just remove it for now.
-  # df_temp = df %>% group_by(id, category) %>% summarise(qty=sum(qty))
-  # df_category_qty_count = df_temp %>% spread(category, qty)
-  # df_category_qty_count[is.na(df_category_qty_count)]=0
-  
   ### Monetary
   # average order quantity | average order price
   df_average_monetary = df %>% group_by(id, ordnum) %>% summarise(qty_sum=sum(qty), ord_value=sum(total_price)) %>% 
                                 group_by(id) %>% summarise(avg_qty = mean(qty_sum), avg_ord_value= mean(ord_value))
+  # total money spent
+  df_total_money = df %>% group_by(id, ordnum) %>% mutate(ord_value=total_price*qty) %>% 
+    group_by(id) %>% summarise(total_money = sum(ord_value))
+  
+  ### Diversity
+  # number of categories purcahsed
+  df_cat_count = df %>% group_by(id) %>% summarise(cat_count = n_distinct(category))
+  
+  ### Enthrophy
+  # how diverse the amount of purchase are from all categories (~0.5 means diverse)
+  df_entrophy = df %>% group_by(id,category) %>% summarise(t_qty = sum(qty)) %>% 
+    mutate(freq = t_qty / sum(t_qty)) %>% group_by(id) %>% 
+    summarise(entrophy = -sum(freq*log(freq)))
+  
+  ###Categories per order:
+  df_average_catcount = df %>% group_by(id, ordnum) %>% summarise(cat_count_ord=n_distinct(category)) %>% 
+    group_by(id) %>% summarise(avg_cats = mean(cat_count_ord))
   
   ### Others
   # build a linear regression and use the slope as the trend
@@ -116,23 +138,6 @@ feature_engineer <- function(df, isTrain=TRUE){
   df_slope$slope = ifelse(df_slope$slope>=3, 3, df_slope$slope)
   df_slope$slope = ifelse(df_slope$slope<=-3, -3, df_slope$slope)
 
-  # Using monthly unique ordnum count. It doesn't work, since the distribution is too skewed.
-  # temp = df %>% mutate(ym = as.yearmon(format(df$orddate,'%Y-%m')))
-  # df_reg = temp %>% group_by(id,ym) %>% summarise(ordnum=n_distinct(ordnum)) %>% arrange(id, ym)
-  # df_slope = df_reg %>% group_by(id) %>% summarise(slope=n())
-  # for(i in 1:dim(df_slope)[1]){
-  #   if(df_slope$slope[i]!=1){
-  #     df_id_filter = df_reg[df_reg$id == df_slope$id[i],]
-  #     fit_id = lm(ordnum~ym, data=df_id_filter)
-  #     df_slope$slope[i] = summary(fit_id)$coefficients[2]
-  #   }else{
-  #     df_slope$slope[i]=0
-  #   }
-  # }
-  # df_slope$slope = scale(df_slope$slope)
-  # df_slope$slope = ifelse(df_slope$slope>=3, 3, df_slope$slope)
-  # df_slope$slope = ifelse(df_slope$slope<=-3, -3, df_slope$slope)
-  
   # calculate the Coefficient of variation using the qty over year 
   # reference: https://en.wikipedia.org/wiki/Coefficient_of_variation
   temp = df %>% group_by(id,orddate) %>% summarise(qty=sum(qty)) %>% arrange(id, orddate)
@@ -143,10 +148,17 @@ feature_engineer <- function(df, isTrain=TRUE){
   df_coeva$coe_va = df_coeva$qty_sd/df_coeva$qty_mean
   df_coeva = df_coeva %>% dplyr::select(id, coe_va)
   
+  # count distinct order date per id
+  df_distinct_orddate = df %>% group_by(id) %>% summarise(distinct_orddate = length(unique(orddate)))
+
+  ### after aug
+  df_after_aug = df %>% group_by(id) %>% mutate(month = substr(orddate, 6, 7)) %>% summarise(aug_after_orders = sum(as.numeric(month)>7))
+  df_aug = df %>% group_by(id) %>% mutate(month = substr(orddate, 6, 7)) %>% summarise(aug_orders = sum(as.numeric(month)==8))
   
   ### Merge all the dataframe together
   df_list = list(df_last_purchase_time, df_order_count, df_average_monetary, 
-                 df_slope, df_coeva)#df_category_qty_count
+                 df_slope, df_coeva, df_cat_count, df_entrophy, df_total_money, df_average_catcount, df_distinct_orddate,
+                 df_after_aug)
   result = Reduce(function(x, y) merge(x, y, all=TRUE), df_list)
 
   ### Keep response variable when doing feature engineer for training data
@@ -155,7 +167,7 @@ feature_engineer <- function(df, isTrain=TRUE){
   if(isTrain==TRUE){
     result = merge(result, df_response, by='id',all.x= TRUE)
   }   
-
+  result$ordperyr <- result$order_count / ((result$total_duration/365)+.1)
   return(result)  
 }
 
@@ -176,76 +188,3 @@ my_cv_glmnet <- function(y, x, alpha){
   return(list(lambda=fitted_min_lambda,
               small.lambda.betas=small.lambda.betas))
 }
-
-get_optimal_p <- function(real_response, predict_fit, p_threshold_list){
-  # get the optimal probability threshold according to CCR / f1_score
-  #
-  # Parameters
-  # ----------
-  # @param real_response: actual response vector
-  # @param predict_fit: predicted probabilty from the model
-  # @param p_threshold_list: list of probability that we want to test on
-  # @return: optimal probability threshold according to CCR
-  
-  max_metric= 0
-  optimal_p = 0
-  
-  
-  for (p in p_threshold_list){
-    pred = rep(0, length(real_response))
-    pred[predict_fit > p]=1
-    pred= factor(pred, levels=c(0,1))
-    # ccr = sum(diag(table(real=real_response, pred)))/ length(real_response)
-    
-    confusion_table = table(real=real_response, pred)
-    
-    sensitivity=confusion_table[2,2]/(confusion_table[2,1]+confusion_table[2,2])
-    specificity=confusion_table[1,1]/(confusion_table[1,1]+confusion_table[1,2])
-    precision=confusion_table[2,2]/(confusion_table[1,2]+confusion_table[2,2])
-    f1_score=2*precision*sensitivity/(precision+sensitivity)
-    # print(paste("f1_score:",f1_score," when p=",p))
-    
-    if (f1_score >= max_metric){
-      max_metric= f1_score
-      optimal_p = p
-    }
-  }
-  
-  return(optimal_p)
-}
-
-calculate_metrics <- function(df,real_response, predict_fit, optimal_p){
-  # a function that calculate all the classification related metrics, including confusion table, auc, ccr, ...etc.
-  #
-  # Parameters
-  # ----------  
-  # @param real_response: actual response vector
-  # @param predict_fit: predicted probabilty from the model
-  # @param optimal_p: optimal probability threshold according to CCR
-  # @return: a list of auc, ccr, sensitivity, precision, f1_score 
-  
-  # generate the vecotr that transform the fitted result into a vector of classified numbers
-  predict_response = rep(0,dim(df)[1])
-  predict_response[predict_fit>optimal_p[1]]=1
-  
-  
-  confusion_table = table(actual = real_response, prediction = predict_response)
-  print(confusion_table)
-  
-  ccr = sum(diag(confusion_table))/ sum(confusion_table)
-  sensitivity=confusion_table[2,2]/(confusion_table[2,1]+confusion_table[2,2])
-  specificity=confusion_table[1,1]/(confusion_table[1,1]+confusion_table[1,2])
-  precision=confusion_table[2,2]/(confusion_table[1,2]+confusion_table[2,2]) 
-  f1_score=2*precision*sensitivity/(precision+sensitivity)
-  
-  plot.roc(real_response, predict_fit, xlab="1-Specificity")
-  
-  my_auc = auc(real_response, predict_fit)
-  
-  return(list(auc=my_auc,
-              ccr=ccr,
-              sensitivity=sensitivity, 
-              specificity=specificity,
-              f1_score=f1_score))
-}
-
